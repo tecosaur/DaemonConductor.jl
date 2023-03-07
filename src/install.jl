e9c3ac1fcffbe86ce83492133004f6d1cb4dbe5e
TEC <git@tecosaur.net>
2023-03-07 22:41:49 +0800

    Add uninstall command

--- src/install.jl
+++ src/install.jl
@@ -36,3 +36,22 @@ function install()
     chmod(binpath, 0o555)
     @info "Done"
 end
+
+function uninstall()
+    if !Sys.islinux()
+        error("$(@__MODULE__)'s service and client are only installed on Linux at the moment")
+    end
+    servicefile = XDG.User.config("systemd", "user", SYSTEMD_SERVICE_NAME * ".service",
+                                  create=true)
+    if ispath(servicefile)
+        @info "Disabling systemd service"
+        run(`systemctl --user disable --now $SYSTEMD_SERVICE_NAME.service`)
+        @info "Removing systemd service file $servicefile"
+        rm(servicefile)
+        run(`systemctl --user daemon-reload`)
+    end
+    binpath = XDG.User.bin(CLIENT_NAME)
+    @info "Removing binary client $binpath"
+    ispath(binpath) && rm(binpath)
+    @info "Done"
+end
