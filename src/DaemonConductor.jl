dcef3fbbfd16ce9ce1bc57cc527c8d8349d8c7fd
TEC <git@tecosaur.net>
2023-06-14 20:44:00 +0800

    Add docstring to module

--- src/DaemonConductor.jl
+++ src/DaemonConductor.jl
@@ -25,4 +25,25 @@ include("workers.jl")
 include("conductor.jl")
 include("install.jl")
 
+@doc """
+    DaemonConductor
+
+# Setup
+
+Install this package anywhere and run `DaemonConductor.install()`.
+Re-run this command after updating `DaemonConductor` and/or Julia itself.
+
+# Configuration
+
+When the daemon starts, it pays attention to the following environmental variables:
+- `JULIA_DAEMON_SERVER` [`$(BaseDirs.User.runtime("julia-daemon.sock"))`] \n
+  The socket to connect to
+- `JULIA_DAEMON_WORKER_ARGS` [`--startup-file=no`] \n
+  Arguments passed to the Julia worker processes
+- `JULIA_DAEMON_WORKER_EXECUTABLE` [`$(joinpath(Sys.BINDIR, "julia"))`] \n
+  Path to the Julia executable used by the workers
+- `JULIA_DAEMON_WORKER_TTL` [`$DEFAULT_WORKER_TTL`] \n
+  Number of seconds a worker should be kept alive for after the last client disconnects
+""" DaemonConductor
+
 end
